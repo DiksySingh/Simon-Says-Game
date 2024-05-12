d6c94c86a8e1877185310791bb7fa6853ff69d93
Dikshant Kumar Singh <singhdikshant24@gmail.com>
2024-05-12 12:07:02 +0530

    Completed the javascript section of the game

--- app.js
+++ app.js
@@ -15,12 +15,14 @@ document.addEventListener('keypress', function(){
 });
 
 function levelUp(){
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
     let randIndex = Math.floor(Math.random() * 3);
     let randColor = btns[randIndex];
     let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
     gameFlash(randBtn);
 }
 
@@ -38,12 +40,37 @@ function userFlash(btn){
     },250);
 }
 
+function checkSeq(idx){
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 300);
+        }
+    }else{
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+}
+
 function btnPressed(){
     let btn = this;
     userFlash(btn);
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkSeq(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click", btnPressed);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
